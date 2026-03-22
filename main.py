14d6732254146c66835fde275c72158db2b96278
Ripu Daman Singh <b23cs1057@iitj.ac.in>
2026-03-22 12:07:24 +0530

    COmpleted first time setup and added basic loadup functions

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ import sqlite3
 import os
 from datetime import datetime
 
-
+#Global entities
 DB_PATH ="pocketpolice.db"
-
+config_dict = {
+    "monthly_budget":"",
+    "per_task_earning":"",
+    "bonus":"",
+    "penalty":"",
+    "deadline":""
+}
 
 #To check if it's the first time opening the app
 def first_time(DB_PATH):
@@ -20,7 +26,7 @@ def first_time_setup(DB_PATH):
 
     while True:
         deadline_string = input("Enter time in HH:MM format(eg: 14:30): ")
-        
+
         try:
             deadline = datetime.strptime(deadline_string, "%H:%M").time()
             break
@@ -39,16 +45,70 @@ def first_time_setup(DB_PATH):
               per_task_earning INT,
               bonus INT,
               penalty INT,
-              deadline TIME
+              deadline TEXT
               )
     ''')
 
     #Creating the config entry
     insert_query = """INSERT INTO config (id, monthly_budget, per_task_earning, bonus, penalty, deadline) VALUES(?,?,?,?,?,?)"""
-    c.execute(insert_query, (1,monthly_budget,per_task_earning,bonus,penalty,deadline))
+    c.execute(insert_query, (1,monthly_budget,per_task_earning,bonus,penalty,deadline_string))
+
+    #Create task table
+    c.execute('''
+            CREATE TABLE IF NOT EXISTS tasks(
+              id INTEGER PRIMARY KEY,
+              task_name TEXT NOT NULL,
+              task_date TEXT NOT NULL,
+              created_at TEXT,
+              completed INTEGER DEFAULT 0 CHECK (completed in (0,1)),
+              completion_time TEXT,
+              reward_granted INTEGER DEFAULT 0 CHECK (reward_granted in (0,1))
+              )
+    ''')
+
+    c.execute('''
+            CREATE TABLE IF NOT EXISTS transactions(
+              id INTEGER PRIMARY KEY,
+              timestamp TEXT,
+              type TEXT NOT NULL,
+              amount INTEGER NOT NULL,
+              description TEXT,
+              reference_id INTEGER
+              )
+    ''')
 
     conn.commit()
     conn.close()
+
+    #Save the values to config dict
+    config_dict["monthly_budget"] = monthly_budget
+    config_dict["per_task_earning"] = per_task_earning
+    config_dict["bonus"] = bonus
+    config_dict["penalty"] = penalty
+    config_dict["deadline"] = deadline_string
+
+    print("First time setup completed.")
+
+#To load necessary data when starting up
+def load_data(DB_PATH):
+
+    #Get the config values from the db
+    conn = sqlite3.connect(DB_PATH)
+    c = conn.cursor()
+
+    c.execute('''
+            SELECT * FROM config
+    ''')
+    row = c.fetchall()
+    config_dict["monthly_budget"] = row[1]
+    config_dict["per_task_earning"] = row[2]
+    config_dict["bonus"] = row[3]
+    config_dict["penalty"] = row[4]
+    config_dict["deadline"] = row[5]
+    
+    conn.commit()
+    conn.close()
+    
 
 def startup(DB_PATH):
     if first_time(DB_PATH):
